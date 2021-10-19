5cde2294ff433a45d2a58ecc17207aed9afb14bf
Meno Hochschild <mhochschild@gmx.de>
2021-10-19 21:03:37 +0200

    update to tzdb v2021d
    See issue https://github.com/MenoData/Time4J/issues/956

--- src/test/java/net/time4j/tz/repo/RepositoryTest.java
+++ src/test/java/net/time4j/tz/repo/RepositoryTest.java
@@ -37,7 +37,7 @@ public class RepositoryTest {
         TZDATA.init();
     }
 
-    private static final String STD_VERSION = "2021c";
+    private static final String STD_VERSION = "2021d";
     private static final ChronoFormatter<Moment> PARSER = Iso8601Format.EXTENDED_DATE_TIME_OFFSET;
 
     private String propertyValue = null;
